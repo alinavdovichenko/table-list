cc5cd3295fb4dcda791090cf2f64a113f7867cdc
Алина Вдовиченко <alinavdovichenko.ru@gmail.com>
2025-04-24 18:04:32 +0500

    5. fix: обновит сортировку поиском.

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,5 +1,5 @@
 import { observer } from 'mobx-react-lite';
-import { Table } from './components/Table';
+import {Table} from './components/Table';
 
 export const App = observer(() => {
   return (
